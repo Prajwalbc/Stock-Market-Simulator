30d1cec2265ea4b23b91e1f91b620d4b2216340c
Prajwal BC <dearprajwal007@gmail.com>
2022-05-28 23:56:53 +0530

    added loading spinner

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./style.css";
+
+function LoadingSpinner() {
+  return (
+    <div className="loading-container">
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1"></div>
+        <div className="sk-cube sk-cube2"></div>
+        <div className="sk-cube sk-cube3"></div>
+        <div className="sk-cube sk-cube4"></div>
+        <div className="sk-cube sk-cube5"></div>
+        <div className="sk-cube sk-cube6"></div>
+        <div className="sk-cube sk-cube7"></div>
+        <div className="sk-cube sk-cube8"></div>
+        <div className="sk-cube sk-cube9"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
