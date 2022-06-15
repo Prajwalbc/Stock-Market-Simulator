a89d334dc12eb34aa80bb801c3f25463ea26c869
Prajwal BC <dearprajwal007@gmail.com>
2022-06-15 23:43:20 +0530

    added getDateTime helper function

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -5,3 +5,19 @@ export function isNullOrWhiteSpaceOrEmpty(str) {
 export function sleep(milliseconds) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
+
+export function getDateTime(ISOdateTime) {
+  const dt = new Date(ISOdateTime);
+
+  const parsedDateTime = new Intl.DateTimeFormat("en-IN", {
+    dateStyle: "short",
+    timeStyle: "short",
+  })
+    .format(dt)
+    .split(",");
+
+  return parsedDateTime;
+}
+
+// let a = new Date().toISOString();
+// console.log(a);
