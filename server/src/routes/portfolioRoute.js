b21755df761bf20fae4145d6315eabd918f480b2
Prajwal BC <dearprajwal007@gmail.com>
2022-06-19 23:02:16 +0530

    fixed porfolio route

--- server/src/routes/portfolioRoute.js
+++ server/src/routes/portfolioRoute.js
@@ -3,7 +3,7 @@ const pool = require("../config/db.js");
 exports.getPortfolios = async (req, res) => {
   try {
     const portfolio = await pool.query(
-      "SELECT id, p_scrip_name, p_date_time, p_no_of_scrips, p_bought_price,t_id  FROM sim_portfolios WHERE u_id = $1",
+      "SELECT id, p_scrip_name, p_direct_scrip_name, p_date_time, p_no_of_scrips, p_bought_price,t_id  FROM sim_portfolios WHERE u_id = $1",
       [req.user.id]
     );
 
